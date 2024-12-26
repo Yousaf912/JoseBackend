e977a3c5bd0eace4d5b14f86e463fcd0bdac1c34
Muhammad Yousaf <yousafva9@gmail.com>
2024-12-26 14:31:06 +0500

    updated

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ App.use(cors(corsOptions));
 App.use(express.json())
 App.use('/',router)
 
-App.listen(port, () => {
-    console.log(`server is running on this port ${port}`);
-    mongoose.connect('mongodb+srv://JoseMart:josemart364855@cluster0.7xkhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
-        console.log('mongodb is connect');
-
-    }).catch((er) => {
-        console.log(er);
+mongoose.connect('mongodb+srv://JoseMart:josemart364855@cluster0.7xkhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+    console.log('mongodb is connect');
+    App.listen(port, () => {
+        console.log(`server is running on this port ${port}`);
+        
     })
+}).catch((er) => {
+    console.log(er);
 })
