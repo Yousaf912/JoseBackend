1c0942d71c6b6374c76739231795e2fb3c273935
Muhammad Yousaf <yousafva9@gmail.com>
2024-12-26 15:30:31 +0500

    updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const App = express();
 const port = process.env.PORT;
 const cors = require("cors");
 const deployurl ='https://josemart.vercel.app';
-
+const mongourl = process.env.MONGO_URL;
 
 
 
@@ -16,8 +16,7 @@ const corsOptions = {
     methods: ['POST', 'GET', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, 
-    preflightContinue: false,
-    optionsSuccessStatus: 204
+   
 };
 
 App.use(cors(corsOptions));
@@ -25,7 +24,7 @@ App.use(cors(corsOptions));
 App.use(express.json())
 App.use('/',router)
 
-mongoose.connect('mongodb+srv://JoseMart:josemart364855@cluster0.7xkhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+mongoose.connect(mongourl).then(() => {
     console.log('mongodb is connect');
     App.listen(port, () => {
         console.log(`server is running on this port ${port}`);
